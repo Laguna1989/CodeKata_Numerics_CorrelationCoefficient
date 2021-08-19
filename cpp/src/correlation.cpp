813ea27bae33aaa3414e11dc904c85f03d734130
Simon Weis <laguna.ws@gmail.com>
2021-08-19 15:22:48 +0200

    Add cpp solution

--- cpp/src/correlation.cpp
+++ cpp/src/correlation.cpp
@@ -3,21 +3,31 @@
 
 double calculate_sum(std::vector<double> const& values)
 {
-    return 42.0;
+    return std::accumulate(values.begin(), values.end(), 0.0);
 }
 
 double calculate_sum_of_multiplies(
     std::vector<double> const& values_x, std::vector<double> const& values_y)
 {
-    return 42.0;
+    return std::inner_product(values_x.cbegin(), values_x.cend(), values_y.begin(), 0.0);
 }
 
 double calculate_sum_of_squares(std::vector<double> const& values)
 {
-    return 42.0;
+    return std::inner_product(values.begin(), values.cend(), values.cbegin(), 0.0);
 }
 
 double correlation(std::vector<double> const& values_x, std::vector<double> const& values_y)
 {
-    return 42.0;
+    auto const n = values_x.size();
+
+    double const sum_xy = calculate_sum_of_multiplies(values_x, values_y);
+    double const sum_x = calculate_sum(values_x);
+    double const sum_y = calculate_sum(values_y);
+
+    double const sum_x_squared = calculate_sum_of_squares(values_x);
+    double const sum_y_squared = calculate_sum_of_squares(values_y);
+
+    return (n * sum_xy - sum_x * sum_y)
+        / sqrt((n * sum_x_squared - sum_x * sum_x) * (n * sum_y_squared - sum_y * sum_y));
 }
