5acaadb5efc76180ddbf78ed062a7b4d72e5579f
Simon Weis <laguna.ws@gmail.com>
2021-08-19 12:23:14 +0200

    Add python solution

--- python/src/correlation.py
+++ python/src/correlation.py
@@ -3,17 +3,26 @@ import numpy as np
 
 
 def calculate_sum(values):
-    return 42.0
+    return sum(values)
 
 
 def calculate_sum_of_squares(values):
-    return 42.0
+    return sum(map(lambda x: x * x, values))
 
 
 def calculate_sum_of_multiplies(values_x, values_y):
-    return 42.0
+    return sum(np.multiply(values_x, values_y))
 
 
 def correlation(values_x, values_y):
     assert len(values_x) == len(values_y)
-    return 42.0
+    n = len(values_x)
+
+    sum_xy = calculate_sum_of_multiplies(values_x, values_y)
+    sum_x = calculate_sum(values_x)
+    sum_y = calculate_sum(values_y)
+
+    sum_x_squared = calculate_sum_of_squares(values_x)
+    sum_y_squared = calculate_sum_of_squares(values_y)
+
+    return (n * sum_xy - sum_x * sum_y) / sqrt((n * sum_x_squared - sum_x ** 2) * (n * sum_y_squared - sum_y ** 2))
